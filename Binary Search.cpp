487a3fbd1a03c351a3f604ab9da08037a42397f7
codshashank1 <96619639+codshashank1@users.noreply.github.com>
2022-04-21 08:24:12 +0530

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,9 +1,14 @@
 //All insights to Binary Search
+
 // 1. Array MUST be sorted.
 // 2. The exit condition of the loop must be carefully scrutinized so that it is exhausted to only 1 element at maximum.
 // 3. Overflow issues with mid index calculation must be manipulated.
+// 4. Typical example of Divide and Conquer Technique.
+
+
 
 //Basic Algorithm
+
 int binarySearch(int A[], int n, int x){
     int start = 0, end = n - 1, mid;
     while(start <= end){
@@ -18,3 +23,27 @@ int binarySearch(int A[], int n, int x){
     }
     return -1; // in case no index matching to x found.
 }
+
+
+
+//Recursive Approach.
+//At any given search space in the sorted array, we would require its start and end position, the array, and 
+//the element to be searched as the arguments of recursive function.
+
+int binarySearch(int a[], int start, int end, int x){
+    // base case
+    if(start > end)
+        return -1;
+
+
+     int mid = start + (end - start) / 2;
+     if(x == a[mid])
+        return mid;
+     else if(x < a[mid])
+       return binarySearch(a, start, mid - 1, x);
+     else 
+        return binarySearch(a, mid + 1, end, x);     
+}
+
+
+
